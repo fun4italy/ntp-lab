224af41f3eefe17ed65ad8facf9cd867a33c8146
fun4italy <fun4italy@gmail.com>
2024-11-05 14:03:51 +0300

    Добавлен юнит-тест

---
+++ Model/UnitTests/Model/HelicopterTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using Model;
+
+namespace UnitTests.Model
+{
+    /// <summary>
+    /// Набор тестов для класса Helicopter
+    /// </summary>
+    [TestFixture]
+    internal class HelicopterTest
+    {
+        /// <summary>
+        /// Тестирование свойства FuelConsumption
+        /// </summary>
+        /// <param name="consumption"></param>
+        [Test]
+        [TestCase(10.0, TestName = "Тестирование FuelConsumption при присваивании 10.0")]
+        public void FuelConsumptionTest(double consumption)
+        {
+            _ = new Helicopter(consumption, 1.0);
+        }
+    }
+}
