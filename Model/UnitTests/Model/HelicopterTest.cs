4e24225d065be2899f71cd498d28c30106d2baec
fun4italy <fun4italy@gmail.com>
2024-11-05 15:24:01 +0300

    Добавлен юнит-тест для метода Helicopter.FuelByDistance()

--- Model/UnitTests/Model/HelicopterTest.cs
+++ Model/UnitTests/Model/HelicopterTest.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 using Model;
 
@@ -15,10 +18,52 @@ namespace UnitTests.Model
         /// </summary>
         /// <param name="consumption"></param>
         [Test]
+        [TestCase(1.0, TestName = "Тестирование FuelConsumption при присваивании 1.0")]
         [TestCase(10.0, TestName = "Тестирование FuelConsumption при присваивании 10.0")]
+        [TestCase(100.0, TestName = "Тестирование FuelConsumption при присваивании 100.0")]
+        [TestCase(double.MaxValue - 1.0, TestName = "Тестирование FuelConsumption при присваивании MaxValue-1.0")]
+        [TestCase(double.MaxValue, TestName = "Тестирование FuelConsumption при присваивании MaxValue")]
+        [TestCase(0.0, TestName = "Тестирование FuelConsumption при присваивании 0.0")]
+        [TestCase(-1.0, TestName = "Тестирование FuelConsumption при присваивании -1.0")]
+        [TestCase(double.MinValue, TestName = "Тестирование FuelConsumption при присваивании MinValue")]
         public void FuelConsumptionTest(double consumption)
         {
-            _ = new Helicopter(consumption, 1.0);
+            if (consumption <= 0)
+            {
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    _ = new Helicopter(consumption, 100.0);
+                });
+            }
+            else
+            {
+                _ = new Helicopter(consumption, 100.0);
+            }
+        }
+
+        /// <summary>
+        /// Тестирование свойства FuelConsumption
+        /// </summary>
+        /// <param name="distance"></param>
+        [Test]
+        [TestCase(100.0, ExpectedResult = 100.0, TestName = "Тестирование FuelByDistance при distance = 100.0")]
+        [TestCase(1000.0, ExpectedResult = 1000.0, TestName = "Тестирование FuelByDistance при distance = 1000.0")]
+        [TestCase(0.0, ExpectedResult = double.NaN, TestName = "Тестирование FuelByDistance при distance = 0.0")]
+        [TestCase(-1.0, ExpectedResult = double.NaN, TestName = "Тестирование FuelByDistance при distance = -1.0")]
+        [TestCase(double.MinValue, ExpectedResult = double.NaN, TestName = "Тестирование FuelByDistance при distance = MinValue")]
+        public double FuelByDistanceTest(double distance)
+        {
+            var obj = new Helicopter(100.0, 100.0);
+
+            if (distance <= 0)
+            {
+                Assert.Throws<ArgumentException>(() => obj.FuelByDistance(distance));
+                return double.NaN;
+            }
+            else
+            {
+                return obj.FuelByDistance(distance);
+            }
         }
     }
 }
